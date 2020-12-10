cb2cb63827f57a6f628f6344555392ed5f4737b2
Paulo Almeida <paulo_machado_a@yahoo.com.br>
2020-12-10 01:01:57 -0300

    continue template

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,13 @@ var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
 
-//Move e compila  Bootstrap scss para Css
+//Move e compila  Bootstrap/Icons scss para Css
 gulp.task('sass', () => {
     return gulp.src(['./node_modules/bootstrap/scss/bootstrap.scss', 
-                     'src/assets/scss/*.scss'])
+                        'node_modules/@fortawesome/fontawesome-free/scss/fontawesome.scss', 
+                        'node_modules/@fortawesome/fontawesome-free/scss/brands.scss', 
+                        'node_modules/@fortawesome/fontawesome-free/scss/solid.scss',
+                        'src/assets/scss/*.scss'])
         .pipe(sass())
         .pipe(gulp.dest('src/assets/css'))
         .pipe(browserSync.stream());
@@ -19,17 +22,24 @@ gulp.task('js', () => {
         .pipe(browserSync.stream());
 });
 
+//Move icons
+gulp.task('icons', function() {
+    return gulp.src('./node_modules/@fortawesome/fontawesome-free/webfonts/*')
+        .pipe(gulp.dest('src/assets/webfonts/'));
+});
 
 //Server para Atualizar front quando houver alguma alteração nos arquivos
-gulp.task('serve', gulp.series(['sass', 'js'], function() {
+gulp.task('serve', gulp.series(['sass', 'js', 'icons'], function() {
     browserSync.init({
         server: "./src"
     });
 
     gulp.watch('src/assets/scss/*.scss', gulp.series(['sass']));
+    gulp.watch("src/assets/webfonts/*", gulp.series(['icons']));
     gulp.watch("src/assets/js/*.js", gulp.series(['js']));
     gulp.watch('src/*.html').on('change', browserSync.reload);
 }));
+
 
 gulp.task('default', gulp.series('serve'));
 
--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,46 +1,56 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-    'use strict';
-    window.addEventListener('load', function () {
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        var forms = document.getElementsByClassName('needs-validation');
-        // Loop over them and prevent submission
-        var validation = Array.prototype.filter.call(forms, function (form) {
-            form.addEventListener('submit', function (event) {
-                if (form.checkValidity() === false) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                }
-                form.classList.add('was-validated');
-            }, false);
-        });
-    }, false);
-})();
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 60){
+        $('header').addClass('header-fixed-in shadow-sm').removeClass('header-fixed-out');
+    }else{
+        $('header').addClass('header-fixed-out').removeClass('header-fixed-in shadow-sm');
+    }
+})
 
-// Fixed top Navbar after scrolling
-if ($(window).width() > 992){
-    $(window).scroll(function (){
-        if($(this).scrollTop() > 40){
-            $('header').addClass('.header-fixed');
-            $('#navbar-top').addClass('fixed-top');
 
-            $('body').css('padding-top', $('.navbar').outerHeight() + 'px');
-        }else{
-            $('header').removeClass('.header-fixed');
-            $('#navbar-top').removeClass('fixed-top');
-            $('body').css('padding-top', '0');
-        }
-    });
-}
 
-// //Loading
-// $('body').append('<div id="preloader"><div id="loader"></div></div>');
-// $('window').on('load', function(){
-//     setTimeout(removeLoading, 5000);
-// })
+// // Example starter JavaScript for disabling form submissions if there are invalid fields
+// (function () {
+//     'use strict';
+//     window.addEventListener('load', function () {
+//         // Fetch all the forms we want to apply custom Bootstrap validation styles to
+//         var forms = document.getElementsByClassName('needs-validation');
+//         // Loop over them and prevent submission
+//         var validation = Array.prototype.filter.call(forms, function (form) {
+//             form.addEventListener('submit', function (event) {
+//                 if (form.checkValidity() === false) {
+//                     event.preventDefault();
+//                     event.stopPropagation();
+//                 }
+//                 form.classList.add('was-validated');
+//             }, false);
+//         });
+//     }, false);
+// })();
 
-// function removeLoading(){
-//     $('#preloader').fadeOut(500, function(){
-//         $('#preloader').remove();
+// // Fixed top Navbar after scrolling
+// if ($(window).width() > 992){
+//     $(window).scroll(function (){
+//         if($(this).scrollTop() > 40){
+//             $('header').addClass('.header-fixed');
+//             $('#navbar-top').addClass('fixed-top');
+
+//             $('body').css('padding-top', $('.navbar').outerHeight() + 'px');
+//         }else{
+//             $('header').removeClass('.header-fixed');
+//             $('#navbar-top').removeClass('fixed-top');
+//             $('body').css('padding-top', '0');
+//         }
 //     });
 // }
+
+// // //Loading
+// // $('body').append('<div id="preloader"><div id="loader"></div></div>');
+// // $('window').on('load', function(){
+// //     setTimeout(removeLoading, 5000);
+// // })
+
+// // function removeLoading(){
+// //     $('#preloader').fadeOut(500, function(){
+// //         $('#preloader').remove();
+// //     });
+// // }
